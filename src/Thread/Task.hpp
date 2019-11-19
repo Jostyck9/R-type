5333c3d5ed5a920d0b8a5b1d74c5f546d1c03051
Hugo <heaugo@gmail.com>
2019-11-19 10:34:24 +0100

    task and other stuff

--- src/Thread/Task.hpp
+++ src/Thread/Task.hpp
@@ -1,9 +1,26 @@
 #ifndef __TASK_HPP__
 # define __TASK_HPP__
 
+template<typename T>
+class Data
+{
+private:
+
+public:
+    Data();
+    ~Data();
+};
+
+template<typename T>
 class Task {
 private:
+    T (*job)(...) task;
+    Data _data;
 public:
+    Task();
+    ~Task();
+
+
 
 };
 
