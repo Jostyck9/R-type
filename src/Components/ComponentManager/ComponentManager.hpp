4314828cf2a88e784764b32d1fc374af5ad1ded8
Bailloux <esther.bailloux@epitech.eu>
2019-11-19 14:06:35 +0100

    Revert "* deleting referencewrappers"
    This reverts commit 1e0df04a255e5a99f495a45c77f42333e30b7f0e.

--- src/Components/ComponentManager/ComponentManager.hpp
+++ src/Components/ComponentManager/ComponentManager.hpp
@@ -34,10 +34,9 @@ namespace ecs::components
          * 
          * @param map 
          * @param idEntity 
-         * @return std::list<std::shared_ptr<IComponent>> 
+         * @return std::list<std::reference_wrapper<std::shared_ptr<IComponent>>> 
          */
-        std::list<std::shared_ptr<IComponent>> extractComponentsFrom(std::multimap<size_t, std::shared_ptr<IComponent>> &map, size_t idEntity);
-        std::list<std::shared_ptr<IComponent>> extractComponentsOfType(std::multimap<size_t, std::shared_ptr<IComponent>> &map, size_t idEntity, const std::type_index type);
+        std::list<std::reference_wrapper<std::shared_ptr<IComponent>>> extractComponentsFrom(std::multimap<size_t, std::shared_ptr<IComponent>> &map, size_t idEntity);
 
     public:
         ComponentManager();
@@ -47,65 +46,65 @@ namespace ecs::components
          * @brief Get the Physic Components from an entity by his id 
          * 
          * @param idEntity 
-         * @return std::list<std::shared_ptr<IComponent>> 
+         * @return std::list<std::reference_wrapper<std::shared_ptr<IComponent>>> 
          */
-        std::list<std::shared_ptr<IComponent>> getPhysicComponents(size_t idEntity) override;
+        std::list<std::reference_wrapper<std::shared_ptr<IComponent>>> getPhysicComponents(size_t idEntity) override;
 
         /**
          * @brief Get the Physic Component object
          * 
          * @param entity
-         * @return std::list<std::shared_ptr<IComponent>> 
+         * @return std::list<std::reference_wrapper<std::shared_ptr<IComponent>>> 
          */
-        virtual std::list<std::shared_ptr<IComponent>> getPhysicComponents(const std::shared_ptr<entities::Entity> &entity) override;
+        virtual std::list<std::reference_wrapper<std::shared_ptr<IComponent>>> getPhysicComponents(const std::shared_ptr<entities::Entity> &entity) override;
 
         /**
          * @brief Get the Display Components from an entity by his id
          * 
          * @param idEntity 
-         * @return std::list<std::shared_ptr<IComponent>> 
+         * @return std::list<std::reference_wrapper<std::shared_ptr<IComponent>>> 
          */
-        std::list<std::shared_ptr<IComponent>> getDisplayComponents(size_t idEntity) override;
+        std::list<std::reference_wrapper<std::shared_ptr<IComponent>>> getDisplayComponents(size_t idEntity) override;
 
         /**
          * @brief Get the Display Component object
          * 
          * @param entity 
-         * @return std::list<std::shared_ptr<IComponent>> 
+         * @return std::list<std::reference_wrapper<std::shared_ptr<IComponent>>> 
          */
-        virtual std::list<std::shared_ptr<IComponent>> getDisplayComponents(const std::shared_ptr<entities::Entity> &entity) override;
+        virtual std::list<std::reference_wrapper<std::shared_ptr<IComponent>>> getDisplayComponents(const std::shared_ptr<entities::Entity> &entity) override;
 
         /**
          * @brief Get the IO Components from an entity by his id
          * 
          * @param idEntity 
-         * @return std::list<std::shared_ptr<IComponent>> 
+         * @return std::list<std::reference_wrapper<std::shared_ptr<IComponent>>> 
          */
-        std::list<std::shared_ptr<IComponent>> getIOComponents(size_t idEntity) override;
+        std::list<std::reference_wrapper<std::shared_ptr<IComponent>>> getIOComponents(size_t idEntity) override;
 
         /**
          * @brief Get The IO Components from an entity
          * 
          * @param entity 
-         * @return std::list<std::shared_ptr<IComponent>> 
+         * @return std::list<std::reference_wrapper<std::shared_ptr<IComponent>>> 
          */
-        std::list<std::shared_ptr<IComponent>> getIOComponents(const std::shared_ptr<entities::Entity> &entity) override;
+        std::list<std::reference_wrapper<std::shared_ptr<IComponent>>> getIOComponents(const std::shared_ptr<entities::Entity> &entity) override;
 
         /**
          * @brief Get the Game Logic Components from an entity by his id
          * 
          * @param idEntity 
-         * @return std::list<std::shared_ptr<IComponent>> 
+         * @return std::list<std::reference_wrapper<std::shared_ptr<IComponent>>> 
          */
-        std::list<std::shared_ptr<IComponent>> getGameLogicComponents(size_t idEntity) override;
+        std::list<std::reference_wrapper<std::shared_ptr<IComponent>>> getGameLogicComponents(size_t idEntity) override;
 
         /**
          * @brief Get the Game Logic Component object
          * 
          * @param entity 
-         * @return std::list<std::shared_ptr<IComponent>> 
+         * @return std::list<std::reference_wrapper<std::shared_ptr<IComponent>>> 
          */
-        std::list<std::shared_ptr<IComponent>> getGameLogicComponents(const std::shared_ptr<entities::Entity> &entity) override;
+        std::list<std::reference_wrapper<std::shared_ptr<IComponent>>> getGameLogicComponents(const std::shared_ptr<entities::Entity> &entity) override;
 
         /**
          * @brief Add a Physic Component object
