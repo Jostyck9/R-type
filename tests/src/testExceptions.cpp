e9ea43ca9c3cdc033d7d9d62c1c5e7573260525f
ClémentDumaine <clement.dumaine@epitech.eu>
2019-11-20 18:33:28 +0100

    Added unit test for where Exceptions

--- tests/src/testExceptions.cpp
+++ tests/src/testExceptions.cpp
@@ -16,6 +16,7 @@ Test(Exceptions, System_Exceptions)
     SystemExceptions test("System not found", "Here");
     
     cr_assert_str_eq(test.what(), "System not found");
+    cr_assert_str_eq(test.where(), "Here");
 }
 
 Test(Exceptions, Server_Exceptions)
@@ -23,6 +24,7 @@ Test(Exceptions, Server_Exceptions)
     ServerExceptions test("Server not found", "Here");
     
     cr_assert_str_eq(test.what(), "Server not found");
+    cr_assert_str_eq(test.where(), "Here");
 }
 
 Test(EntityExceptions, Entity_Exceptions)
@@ -30,6 +32,7 @@ Test(EntityExceptions, Entity_Exceptions)
     EntityExceptions test("Entity not found", "Here");
     
     cr_assert_str_eq(test.what(), "Entity not found");
+    cr_assert_str_eq(test.where(), "Here");
 }
 
 Test(ComponentExceptions, Component_Exceptions)
@@ -37,6 +40,7 @@ Test(ComponentExceptions, Component_Exceptions)
     ComponentExceptions test("Component not found", "Here");
     
     cr_assert_str_eq(test.what(), "Component not found");
+    cr_assert_str_eq(test.where(), "Here");
 }
 
 Test(ECSExceptions, ECS_Exceptions)
@@ -44,4 +48,5 @@ Test(ECSExceptions, ECS_Exceptions)
     ECSExceptions test("ECS not found", "Here");
     
     cr_assert_str_eq(test.what(), "ECS not found");
+    cr_assert_str_eq(test.where(), "Here");
 }
