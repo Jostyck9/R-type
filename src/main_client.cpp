3e30df9a0523a2d4264a983b22c0a37196150d2b
Romane <romane.bezier@epitech.eu>
2019-11-25 17:52:22 +0100

    Exceptions

--- src/main_client.cpp
+++ src/main_client.cpp
@@ -5,13 +5,22 @@
 ** main_client.cpp
 */
 
+#include <iostream>
+#include "RTypeExceptions.hpp"
 #include "Rtype.hpp"
 
 int main()
 {
-    Rtype rtype;
+    try {
+        Rtype rtype;
 
-    rtype.start();
-    rtype.stop();
+        rtype.start();
+        rtype.stop();
+    } catch (const RTypeExceptions &e) {
+        std::cerr << e.what() << std::endl;
+        std::cerr << "In file: " << e.where() << std::endl;
+    } catch (const std::exception &e) {
+        std::cerr << e.what();
+    }
     return 0;
 }
