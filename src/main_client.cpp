40a19b1b58e79afbc462e485ae1b5ed127d26dd8
Romane <romane.bezier@epitech.eu>
2019-11-25 17:39:48 +0100

    Move resources & Rtype class

--- src/main_client.cpp
+++ src/main_client.cpp
@@ -5,46 +5,13 @@
 ** main_client.cpp
 */
 
-#include "EntityManager.hpp"
-#include "EntityFactory.hpp"
-#include "ComponentManager.hpp"
-#include "SystemManager.hpp"
-#include "Display/DisplaySystem.hpp"
-#include "Display/PhysicsSystem.hpp"
-#include "Physics/Position.hpp"
-#include "Physics/Velocity.hpp"
-#include "TestEntity.hpp"
-#include "IRenderManager.hpp"
-#include "SFMLRenderManager.hpp"
-
-using namespace ecs::components;
-using namespace ecs::system;
-using namespace ecs::entities;
+#include "Rtype.hpp"
 
 int main()
 {
-    bool isPlaying = false;
-	ecs::SFMLRenderManager render;
-    std::shared_ptr<IComponentManager> componentManager = std::make_shared<ComponentManager>();
-    std::shared_ptr<IEntityManager> entityManager = std::make_shared<EntityManager>(componentManager);
-    std::shared_ptr<SystemManager> systemManager = std::make_shared<SystemManager>(entityManager, componentManager);
-    std::shared_ptr<IEntityFactory> factory = std::make_shared<EntityFactory>(entityManager, componentManager);
-	// ecs::SFMLRenderManager* render = new ecs::SFMLRenderManager();
-    // std::shared_ptr<IRenderManager> render = std::make_shared<SFMLRenderManager>();
+    Rtype rtype;
 
-    systemManager->addSystem(std::make_shared<DisplaySystem>(entityManager, componentManager, systemManager->getEntitiesToDelete()));
-    systemManager->setRenderManager(&render);
-    isPlaying = true;
-    factory->addEntityConstructor(std::make_shared<TestEntity>());
-    factory->createEntity("Test");
-    factory->createEntity("Test");
-    factory->createEntity("Test");
-    render.init();
-    render.graphicsUpdate();
-    while (isPlaying == true) {
-        systemManager->updateAll();
-        isPlaying = render.eventUpdate();
-    }
-    render.terminate();
+    rtype.start();
+    rtype.stop();
     return 0;
 }
