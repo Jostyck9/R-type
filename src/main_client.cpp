0c0bc9de37ad2dbfb7da865b4831fec2fcb4ebed
Bailloux <esther.bailloux@epitech.eu>
2019-11-25 14:34:47 +0100

    * now update game and displayable sprite every 2 sec

--- src/main_client.cpp
+++ src/main_client.cpp
@@ -32,6 +32,7 @@ int main()
 	// ecs::SFMLRenderManager* render = new ecs::SFMLRenderManager();
     // std::shared_ptr<IRenderManager> render = std::make_shared<SFMLRenderManager>();
 
+    systemManager->addSystem(std::make_shared<DisplaySystem>(entityManager, componentManager, systemManager->getEntitiesToDelete()));
     systemManager->setRenderManager(&render);
     isPlaying = true;
     factory->addEntityConstructor(std::make_shared<TestEntity>());
@@ -41,8 +42,8 @@ int main()
     render.init();
     render.graphicsUpdate();
     while (isPlaying == true) {
-        isPlaying = render.eventUpdate();
         systemManager->updateAll();
+        isPlaying = render.eventUpdate();
     }
     render.terminate();
     return 0;
