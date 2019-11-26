2ba5a29f0576d66a76210e23a12ba322e37a43ce
Hugo <hugo.berthome@epitech.eu>
2019-11-27 00:19:08 +0100

    adding packet datagram

--- src/Network/PacketManager.hpp
+++ src/Network/PacketManager.hpp
@@ -8,25 +8,72 @@
 #ifndef PACKETMANAGER_HPP
 #define PACKETMANAGER_HPP
 
-class PacketManager {
+#include <iostream>
+
+class PacketManager
+{
 public:
-    enum state {
-        hanshake,
-        command,
-        update,
-    };
-    struct data
+    enum componentType : char
     {
-        union
-        {
-            char rawData[1024];
-            struct {
-                state status;
-                int index;
-                char name[20];
-            } information;
+        POSITION,
+        ROTATION
+    };
+
+private:
+    struct Component
+    {
+        componentType type;
+        union {
+            struct
+            {
+                int x;
+                int y;
+            } _position;
+
+            struct
+            {
+                int radAngle;
+            } _rotation;
         };
     };
+
+public:
+    enum
+    {
+        MAX_LENGTH = 1024,
+        MAX_COMPONENTS = 7,
+    };
+
+private:
+    struct Entity
+    {
+        size_t id;
+        PacketManager::Component components[MAX_COMPONENTS];
+
+    } test;
+
+public:
+    enum
+    {
+        MAX_ENTITIES = MAX_LENGTH / sizeof(Entity)
+    };
+
+private:
+    struct Data
+    {
+        char nbrEntities;
+        Entity entities[MAX_ENTITIES];
+    };
+
+public:
+    struct PacketData
+    {
+        union {
+            char rawData[MAX_LENGTH];
+            Data data;
+        };
+    } packet;
+
     PacketManager();
     ~PacketManager();
 };
