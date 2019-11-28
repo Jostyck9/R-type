f9cda5336462fce06cdcd95ecce04c7ec305e4f4
Romane <romane.bezier@epitech.eu>
2019-11-28 14:04:23 +0100

    Change path

--- src/ResourceManager/RtypeResources.cpp
+++ src/ResourceManager/RtypeResources.cpp
@@ -9,9 +9,9 @@ namespace ecs {
     RtypeResources::RtypeResources()
     {
         try {
-            loadTexture("background", "../resources/background.png");
-            loadTexture("test", "../resources/r-typesheet1.gif");        
-            loadTexture("player", "../resources/player.png");
+            loadTexture("background", "resources/background.png");
+            loadTexture("test", "resources/r-typesheet1.gif");
+            loadTexture("player", "resources/player.png");
         } catch (const std::exception &e) {
             throw e;
         }
