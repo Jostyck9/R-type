824936bc0f65bed7f94bf52c24f19a87eafe6d6d
Hugo <hugo.berthome@epitech.eu>
2019-11-29 16:53:34 +0100

    adding factory wrapper

---
+++ src/FactoriesWrapper/FactoriesWrapper.cpp
@@ -0,0 +1,34 @@
+/*
+** EPITECH PROJECT, 2019
+** R-type
+** File description:
+** FactoriesWrapper.cpp
+*/
+
+#include "FactoriesWrapper.hpp"
+
+namespace ecs
+{
+
+FactoriesWrapper::FactoriesWrapper(
+    std::shared_ptr<ManagerWrapper> &managerWrapper,
+    std::shared_ptr<ecs::system::ISystemManager> &systemManager) : _entityFactory(std::make_shared<ecs::entities::EntityFactory>(managerWrapper->getEntityManager(), managerWrapper->getComponentManager())),
+                                                                   _systemFactory(std::make_shared<ecs::system::SystemFactory>(managerWrapper, systemManager))
+{
+}
+
+FactoriesWrapper::~FactoriesWrapper()
+{
+}
+
+std::shared_ptr<ecs::system::ISystemFactory> &FactoriesWrapper::getSystemFactory()
+{
+    return _systemFactory;
+}
+
+std::shared_ptr<ecs::entities::IEntityFactory> &FactoriesWrapper::getEntityFactory()
+{
+    return _entityFactory;
+}
+
+} // namespace ecs
