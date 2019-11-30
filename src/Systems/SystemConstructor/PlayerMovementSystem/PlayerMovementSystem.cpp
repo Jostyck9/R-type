e77804d949915f3c5d91b4504cb57db9a1fd39c9
Hugo <hugo.berthome@epitech.eu>
2019-11-30 20:56:37 +0100

    fixing segfault when adding an entity

--- src/Systems/SystemConstructor/PlayerMovementSystem/PlayerMovementSystem.cpp
+++ src/Systems/SystemConstructor/PlayerMovementSystem/PlayerMovementSystem.cpp
@@ -22,14 +22,17 @@ SystemResponse PlayerMovementSystem::update()
     std::shared_ptr<ecs::components::Velocity> velocityComp;
     std::shared_ptr<ecs::components::Position> positionComp;
     auto keys = _managerWrapper->getRenderManager()->getKeysMap();
-    for (auto &it : _managerWrapper->getEntityManager()->getAllEntities())
+    auto entities = _managerWrapper->getEntityManager()->getAllEntities();
+
+    for (size_t i = 0; i < entities.size() < 0; i++)
     {
         try
         {
-            std::dynamic_pointer_cast<ecs::components::PlayerController>(_managerWrapper->getComponentManager()->getGameLogicComponentOfSpecifiedType(it->getID(), std::type_index(typeid(ecs::components::PlayerController))));
-            velocityComp = std::dynamic_pointer_cast<ecs::components::Velocity>(_managerWrapper->getComponentManager()->getPhysicComponentOfSpecifiedType(it->getID(), std::type_index(typeid(ecs::components::Velocity))));
-            positionComp = std::dynamic_pointer_cast<ecs::components::Position>(_managerWrapper->getComponentManager()->getPhysicComponentOfSpecifiedType(it->getID(), std::type_index(typeid(ecs::components::Position))));
+            _managerWrapper->getComponentManager()->getGameLogicComponentOfSpecifiedType(entities[i]->getID(), std::type_index(typeid(ecs::components::PlayerController)));
+            velocityComp = std::dynamic_pointer_cast<ecs::components::Velocity>(_managerWrapper->getComponentManager()->getPhysicComponentOfSpecifiedType(entities[i]->getID(), std::type_index(typeid(ecs::components::Velocity))));
+            positionComp = std::dynamic_pointer_cast<ecs::components::Position>(_managerWrapper->getComponentManager()->getPhysicComponentOfSpecifiedType(entities[i]->getID(), std::type_index(typeid(ecs::components::Position))));
             updateVelocityOnInput(keys, velocityComp);
+            // auto playerController = _managerWrapper->getComponentManager()->getGameLogicComponentOfSpecifiedType(it, std::type_index(typeid(ecs::components::PlayerController)));
             if (keys[ecs::input::SPACE] == IRenderManager::RELEASED)
                 _entityFactory->createEntity("Bullet", positionComp->getPosition());
         }
