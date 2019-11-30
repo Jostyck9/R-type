e9a53288977778edce09a458dedd38b2cd683446
Hugo <hugo.berthome@epitech.eu>
2019-11-30 12:35:09 +0100

    set res in packet

--- src/Network/PacketManager.cpp
+++ src/Network/PacketManager.cpp
@@ -147,6 +147,7 @@ void PacketManager::setMsg(const std::string &msg)
 {
     size_t size = MAX_MSG_LENGTH;
 
+    setRes(false);
     if (msg.size() < MAX_MSG_LENGTH)
         size = msg.size();
     std::strncpy(packet.data._msg, msg.c_str(), size);
@@ -168,6 +169,7 @@ const PacketManager::KeysPressed PacketManager::getKeys() const
 int PacketManager::addKey(const ecs::input::Key keyPressed)
 {
     setCmd(UPDATE);
+    setRes(true);
     if (packet.data._keys.size == MAX_KEYS)
         return -1;
     packet.data._keys.list[static_cast<int>(packet.data._keys.size)] = keyPressed;
