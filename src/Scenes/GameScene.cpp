2f57bc68da1dc0b2e18cf253dfc7c608d6f5cc75
Hugo <hugo.berthome@epitech.eu>
2019-11-30 15:47:19 +0100

    fixing nullptr in colision system

--- src/Scenes/GameScene.cpp
+++ src/Scenes/GameScene.cpp
@@ -29,7 +29,7 @@ ecs::GameScene::GameScene(std::shared_ptr<Ecs> &ecs) : _ecs(ecs)
     _ecs->getSystemManager()->addSystem(std::make_shared<system::EnnemiesMovementSystem>(_ecs->getManagerWrapper(), _ecs->getEntityFactory(), _ecs->getSystemManager()->getEntitiesToDelete()));
     _ecs->getSystemManager()->addSystem(std::make_shared<system::AnimationSystem>(_ecs->getManagerWrapper(), _ecs->getEntityFactory(), _ecs->getSystemManager()->getEntitiesToDelete()));
   
-    // _ecs->getEntityFactory()->createEntity("TestBackground");
+    _ecs->getEntityFactory()->createEntity("TestBackground");
     _ecs->getEntityFactory()->createEntity("Player");
     _ecs->getEntityFactory()->createEntity("Bullet");
     _ecs->getEntityFactory()->createEntity("Ennemy");
